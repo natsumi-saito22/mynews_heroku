af1fb3d17e786fda952c3964fdbd53e8f9fa7bbe
”natsumi_saito” <natsumi.saito.2214@gmail.com>
2020-02-09 12:37:09 +0000

    Laravel_assigment13

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -8,6 +8,37 @@
         <div class="row">
             <div class="col-md-8 mx-auto">
                 <h2>Myプロフィール</h2>
+                <form action="{{ action('Admin\ProfileController@create')}}" method="post" enctype="multipart/form-data">
+                  
+                  
+                  <div class="form-group row">
+                      <label class="col-md-2">name</label>
+                      <div class="col-md-8">
+                          <textarea class="form-control" name="body" rows="1" >{{ old('body') }}</textarea>
+                      </div>
+                  </div> 
+                  
+                  <div class="form-group row">
+                      <label class="col-md-2">gender</label>
+                      <div class="col-md-8">
+                          <textarea class="form-control" name="body" rows="1">{{ old('body') }}</textarea>
+                      </div>
+                  </div>
+
+                  <div class="form-group row">
+                　　　<label class="col-md-2">hobby</label>
+                　　　<div class="col-md-10">
+                　　　 <textarea class="form-control" name="body" rows="3">{{ old('body') }}</textarea>
+                　　　</div>
+                  </div>
+                  
+                  <div class="form-group row">
+                      <label class="col-md-2">intoroduction</label>
+                      <div class="col-md-10">
+                          <textarea class="form-control" name="body" rows="10">{{ old('body') }}</textarea>
+                      </div>
+                  </div>
+                
             </div>
         </div>
     </div>
