ef5a9055750119a8424e2362bf9d7614f405caad
”natsumi_saito” <natsumi.saito.2214@gmail.com>
2020-02-10 17:27:31 +0000

    Laravel14_assigment

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -10,7 +10,13 @@
                 <h2>Myプロフィール</h2>
                 <form action="{{ action('Admin\ProfileController@create')}}" method="post" enctype="multipart/form-data">
                   
-                  
+                 @if (count($errors) > 0)
+                        <ul>
+                            @foreach($errors->all() as $e)
+                                <li>{{ $e }}</li>
+                            @endforeach
+                        </ul>
+                 @endif 
                   <div class="form-group row">
                       <label class="col-md-2">name</label>
                       <div class="col-md-4">
@@ -38,7 +44,7 @@
                           <textarea class="form-control" name="body" rows="10">{{ old('body') }}</textarea>
                       </div>
                   </div>
-                
+                <input type="submit" class="btn btn-primary" value="更新">
             </div>
         </div>
     </div>
