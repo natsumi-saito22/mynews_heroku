954f742ad0715be0fa2e314bb0de74331bfd0c0a
”natsumi_saito” <natsumi.saito.2214@gmail.com>
2020-02-18 08:33:56 +0000

    heroku5

--- database/migrations/2020_02_16_081613_create_history2s_table.php
+++ database/migrations/2020_02_16_081613_create_history2s_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateHistories2Table extends Migration
+class CreateHistory2sTable extends Migration
 {
     /**
      * Run the migrations.
